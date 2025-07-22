a6ebc6066d388ae20f28cae6fe5544bb72a5d61b
wizardrad@gmail.com <wizardrad@gmail.com>
2025-07-22 23:09:42 +0500

    books order test

--- client.py
+++ client.py
@@ -1,26 +1,88 @@
 import requests
 
-params = {"q": "test query"}
+# params = {"q": "test query"}
+#
+# item_data = {
+#     "name": "New Phone",
+#     "price": 599.99,
+#     "quantity": 10
+# }
+# response = requests.post(f"http://127.0.0.1:8000/items/", json=item_data)
+# print("POST /items/ response:", response.json())
+#
+# response = requests.get("http://127.0.0.1:8000/items/")
+# print("GET /items/ response:", response.json())
+#
+# update_item_data = {
+#     "name": "New Phone",
+#     "price": "649.00",
+#     "quantity": 5
+# }
+#
+# response = requests.put("http://127.0.0.1:8000/items/New Phone", json=update_item_data)
+# print("PUT /items/{item_name}/ response:", response.json())
+#
+# response = requests.delete("http://127.0.0.1:8000/items/New Phone")
+# print("DELETE /items/{item_name}/ response:", response.json())
 
-item_data = {
-    "name": "New Phone",
-    "price": 599.99,
-    "quantity": 10
+
+books_data = [
+    {
+        "title": "Thinking Java",
+        "author": "Bruce Eckel",
+        "pages": 600
+    },
+    {
+        "title": "Clean Code",
+        "author": "Robert Martin",
+        "pages": 464
+    },
+    {
+        "title": "Python Crash Course",
+        "author": "Eric Matthes",
+        "pages": 560
+    }
+]
+
+updated_book = {
+    "title": "SQL Quick start guide",
+    "author": "Shields",
+    "pages": 223
 }
-response = requests.post(f"http://127.0.0.1:8000/items/", json=item_data)
-print("POST /items/ response:", response.json())
 
-response = requests.get("http://127.0.0.1:8000/items/")
-print("GET /items/ response:", response.json())
+for book in books_data:
+    response = requests.post("http://127.0.0.1:8000/books", json=book)
+    print("POST /books/ response:", response.json())
 
-update_item_data = {
-    "name": "New Phone",
-    "price": "649.00",
-    "quantity": 5
-}
+print()
 
-response = requests.put("http://127.0.0.1:8000/items/New Phone", json=update_item_data)
-print("PUT /items/{item_name}/ response:", response.json())
+response = requests.get("http://127.0.0.1:8000/books/")
+for book in response.json()["books"]:
+    print(f"GET /books/ response: {book['title']} by {book['author']}")
 
-response = requests.delete("http://127.0.0.1:8000/items/New Phone")
-print("DELETE /items/{item_name}/ response:", response.json())
+print()
+
+response = requests.get("http://127.0.0.1:8000/books/2")
+print("GET /books/book_id response:", response.json())
+
+print()
+
+response = requests.put("http://127.0.0.1:8000/books/1", json=updated_book)
+print("PUT /books/book_id response:", response.json())
+
+print()
+
+response = requests.get("http://127.0.0.1:8000/books/")
+for book in response.json()["books"]:
+    print(f"GET /books/ response: {book['title']} by {book['author']}")
+
+print()
+
+response = requests.delete("http://127.0.0.1:8000/books/2")
+print("DELETE /books/book_id response:", response.json())
+
+print()
+
+response = requests.get("http://127.0.0.1:8000/books/")
+for book in response.json()["books"]:
+    print(f"GET /books/ response: {book['title']} by {book['author']}")
--- main.py
+++ main.py
@@ -1,50 +1,47 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    quantity: int
+class Book(BaseModel):
+    title: str = Field(..., min_length=3)
+    author: str
+    pages: int = Field(..., ge=20)
 
 
-items_db = []
+books_db = []
 
 
-@app.get("/")
-def read_root():
-    return {"message": "wtf, world?"}
+@app.post("/books/")
+def create_book(book: Book):
+    books_db.append(book)
+    return {"message": f"Book {book.title} was created successfully", "book": book}
 
 
-@app.get("/items/")
-def get_all_items():
-    return {"message": "All items", "items": items_db}
+@app.get("/books/")
+def get_all_books():
+    return {"books": books_db}
 
 
-@app.post("/items/")
-def create_item(item: Item):
-    items_db.append(item)
-    return {"message": f"Item {item.name} created successfully.", "item": item}
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    if book_id < 0 or book_id >= len(books_db):
+        raise HTTPException(status_code=404, detail="Book not found")
+    return books_db[book_id]
 
 
-@app.put("/items/{item_name}")
-def update_item(item_name: str, updated_item: Item):
-    for i, item in enumerate(items_db):
-        if item.name == item_name:
-            if updated_item.name != item_name:
-                raise HTTPException(status_code=400, detail="Item name in URL and body must match")
-            items_db[i] = updated_item
-            return {"message": f"Item {item_name} updated successfully.", "item": updated_item}
-    raise HTTPException(status_code=404, detail="Item not found")
+@app.put("/books/{book_id}")
+def update_book(book_id: int, updated_book: Book):
+    if book_id < 0 or book_id >= len(books_db):
+        raise HTTPException(status_code=404, detail="Book not found")
+    books_db[book_id] = updated_book
+    return {"message": f"Book {book_id} updated successfully", "book": updated_book}
 
 
-@app.delete("/items/{item_name}")
-def delete_item(item_name: str):
-    for i, item in enumerate(items_db):
-        if item.name == item_name:
-            deleted_item = items_db.pop(i)
-            return {"message": f"Item {item_name} deleted successfully.", "item:": deleted_item}
-    raise HTTPException(status_code=404, detail=f"Item {item_name} not found for deletion")
-
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int):
+    if book_id < 0 or book_id >= len(books_db):
+        raise HTTPException(status_code=404, detail="Book not found")
+    deleted_book = books_db.pop(book_id)
+    return {"message": f"Book {book_id} deleted successfully", "book": deleted_book}
